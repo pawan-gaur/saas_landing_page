0985087b66cc62ff0b662e8d72db13f6a6d03270
pawan-gaur <pawangaur001@gmail.com>
2024-11-04 21:35:20 +0530

    Commit code for features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Header from "./sections/Header"
+import Features from "./sections/Features";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
-      <Header/>
+      <Header />
+      <Hero />
+      <Features />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
